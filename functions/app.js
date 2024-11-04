ae4e162c6aa02693b9ea4260e2ea25a5041cdf7d
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2024-11-03 16:20:43 -0800

    filter method updated as api Unnamed was uncapatlized

--- functions/app.js
+++ functions/app.js
@@ -36,7 +36,7 @@ app.post(
       if (name === "drop") {
         const locations = await axios.get("https://fortnite-api.com/v1/map");
         let drops = locations.data.data.pois;
-        drops = drops.filter((el) => !el.id.includes("UnNamed"));
+        drops = drops.filter((el) => !el.id.includes("Unnamed"));
         const random = Math.floor(Math.random() * drops.length);
         const suggestedLocation = drops[random].name;
 
